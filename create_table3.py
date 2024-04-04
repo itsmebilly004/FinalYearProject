7074897275fd682922c3b4f5845c763369e76e5f
itsmebilly004 <wanyamabill4@gmail.com>
2024-04-05 01:47:04 +0300

    files commited

---
+++ create_table3.py
@@ -0,0 +1,9 @@
+import sqlite3
+
+conn = sqlite3.connect('database.db')
+print("Connected to database successfully")
+
+conn.execute('CREATE TABLE listings (id INTEGER PRIMARY KEY AUTOINCREMENT,location VARCHAR(35) NOT NULL, type VARCHAR(35) NOT NULL, availability text(20) NOT NULL, amount INTEGER NOT NULL,contact INTEGER NOT NULL, image_path TEXT NOT NULL )')
+print("Created table successfully!")
+
+conn.close()
